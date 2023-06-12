799f7278c59e4fdb22fea4ae4b6c2968a8f3d1dc
GreenCobalt <34927146+GreenCobalt@users.noreply.github.com>
2023-06-12 01:46:19 +0000

    disable discordbotlist.com: 403

--- utils/heartbeat.js
+++ utils/heartbeat.js
@@ -38,36 +38,38 @@ function updateWebsites(client) {
         users += g.memberCount;
     });
 
-	axios.post('https://top.gg/api/bots/837538034089590804/stats', { 
+	axios.post('https://top.gg/api/bots/837538034089590804/stats', {
 			server_count: guilds.size
-		}, { headers: { 
-			'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgzNzUzODAzNDA4OTU5MDgwNCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjc0Nzc4NDQ3fQ.bcd9q2maE-wxX561UOyKMsphY1rEEbjq2vghrR02Zbo' 
-		}}) .then((res) => {
+		}, { headers: {
+			'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgzNzUzODAzNDA4OTU5MDgwNCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjc0Nzc4NDQ3fQ.bcd9q2maE-wxX561UOyKMsphY1rEEbjq2vghrR02Zbo'
+		}}).then((res) => {
 				log.debug(`Sent bot stats to top.gg: ${guilds.size} servers`);
 		}).catch((error) => {
 				log.warn(`Failed to send stats to top.gg: ${error}`);
 		});
-		
-	axios.post('https://discord.bots.gg/api/v1/bots/946107355316252763/stats', { 
+
+	axios.post('https://discord.bots.gg/api/v1/bots/946107355316252763/stats', {
 			guildCount: guilds.size
-		}, { headers: { 
-			'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJhcGkiOnRydWUsImlkIjoiNDQxMDQwMTYxNTg5OTUyNTYyIiwiaWF0IjoxNjc0Nzc4MjIwfQ.wzG8nw9yNM2MbOoewdWerALmlAlbxzTsU66ypbnMrAM' 
+		}, { headers: {
+			'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJhcGkiOnRydWUsImlkIjoiNDQxMDQwMTYxNTg5OTUyNTYyIiwiaWF0IjoxNjc0Nzc4MjIwfQ.wzG8nw9yNM2MbOoewdWerALmlAlbxzTsU66ypbnMrAM'
 		}}) .then((res) => {
 				log.debug(`Sent bot stats to discord.bots.gg: ${guilds.size} servers`);
 		}).catch((error) => {
 				log.warn(`Failed to send stats to discord.bots.gg: ${error}`);
 		});
-		
-	axios.post('https://discordbotlist.com/api/v1/bots/946107355316252763/stats', { 
+
+	/*
+	axios.post('https://discordbotlist.com/api/v1/bots/946107355316252763/stats', {
 			guilds: guilds.size,
 			users: users
-		}, { headers: { 
+		}, { headers: {
 			'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0IjoxLCJpZCI6Ijk0NjEwNzM1NTMxNjI1Mjc2MyIsImlhdCI6MTY3NDc4MDUzN30._FVH8RJKAwAwptqUsLyXWXMjHIrP-U5fliAVloE5uQA' 
 		}}) .then((res) => {
 				log.debug(`Sent bot stats to discordbotlist.com: ${guilds.size} servers`);
 		}).catch((error) => {
 				log.warn(`Failed to send stats to discordbotlist.com: ${error}`);
 		});
+	*/
 }
 
 function update(client, dlS, dlF, dlFReasons) {
